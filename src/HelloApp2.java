874993487148af22ea5d787faeaa3bd537c48132
rishi-047 <aaryarishi47@gmail.com>
2026-03-19 03:45:36 +0530

    Add HelloApp2

---
+++ src/HelloApp2.java
@@ -0,0 +1,6 @@
+public class HelloApp2 {
+    public static void main(String[] args) {
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
+    }
+}
